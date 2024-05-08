8b70a63d66a09177a9e3e7d865d026dfa42c8033
James Coliz <jcoliz@outlook.com>
2024-05-08 09:18:00 -0700

    feat: Making simple squares

--- LogoSlideMaker/Program.cs
+++ LogoSlideMaker/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ShapeCrawler;
+
+var pres = new Presentation();
+var slide = pres.Slides[0];
+var shapes = slide.Shapes;
+
+const double dpi = 96.0;
+
+var x = 1.2;
+var y = 1.12;
+var width = 5.0;
+var x_period_7 = width / 6.0;
+var x_period_8 = width / 7.0;
+var y_period = 1;
+
+for(int i = 0; i < 7; i++)
+{
+    shapes.AddRectangle(x:(int)((x + i * x_period_7)*dpi), y:(int)(y*dpi), width:(int)(0.22*dpi), height:(int)(0.22*dpi));
+}
+
+for(int i = 0; i < 8; i++)
+{
+    shapes.AddRectangle(x:(int)((x + i * x_period_8)*dpi), y:(int)((y + y_period)*dpi), width:(int)(0.22*dpi), height:(int)(0.22*dpi));
+}
+
+
+pres.SaveAs("out/1.pptx");
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Logo Slide Maker
 
 This tool, when completed, will create a powerpoint logo slide.
+
+## Using
+
+https://github.com/ShapeCrawler/ShapeCrawler/wiki/Examples
