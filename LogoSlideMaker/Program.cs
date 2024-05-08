e3ce4c083905599a56f368cb6a13f26e74d9b196
James Coliz <jcoliz@outlook.com>
2024-05-08 09:21:48 -0700

    feat: Move icons to be on centers

--- LogoSlideMaker/Program.cs
+++ LogoSlideMaker/Program.cs
@@ -12,15 +12,16 @@ var width = 5.0;
 var x_period_7 = width / 6.0;
 var x_period_8 = width / 7.0;
 var y_period = 1;
+var icon_size = 0.25;
 
 for(int i = 0; i < 7; i++)
 {
-    shapes.AddRectangle(x:(int)((x + i * x_period_7)*dpi), y:(int)(y*dpi), width:(int)(0.22*dpi), height:(int)(0.22*dpi));
+    shapes.AddRectangle(x:(int)((x + i * x_period_7 - icon_size / 2)*dpi), y:(int)((y - icon_size / 2)*dpi), width:(int)(icon_size*dpi), height:(int)(icon_size*dpi));
 }
 
 for(int i = 0; i < 8; i++)
 {
-    shapes.AddRectangle(x:(int)((x + i * x_period_8)*dpi), y:(int)((y + y_period)*dpi), width:(int)(0.22*dpi), height:(int)(0.22*dpi));
+    shapes.AddRectangle(x:(int)((x + i * x_period_8 - icon_size / 2)*dpi), y:(int)((y + y_period - icon_size / 2)*dpi), width:(int)(icon_size*dpi), height:(int)(icon_size*dpi));
 }
 
 
