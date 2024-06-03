ea86af4b414a7552af634adf24079f683a002ad0
James Coliz <jcoliz@outlook.com>
2024-06-03 16:21:09 -0700

    test: Layout one logo

--- LogoSlideMaker.Tests/LayoutTests.cs
+++ LogoSlideMaker.Tests/LayoutTests.cs
@@ -12,6 +12,36 @@ public class LayoutTests
         var variant = new Variant();
         var layout = new Layout.Layout(definition, variant);
 
+        layout.Populate();
+
         Assert.That(layout, Is.Empty);
     }
+
+    [Test]
+    public void One()
+    {
+        var definition = new Definition() 
+        {
+            Rows = 
+            [
+                new Row()
+                {
+                    Logos = [ "test" ]
+                }
+
+            ],
+            Logos = 
+            { 
+                { "test", new Logo() } 
+            },
+
+        };
+        var variant = new Variant();
+        var layout = new Layout.Layout(definition, variant);
+
+        layout.Populate();
+
+        Assert.That(layout, Has.Count.EqualTo(1));
+    }
+
 }
