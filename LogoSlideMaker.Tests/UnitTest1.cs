d2b0a1f3898d78e68b8d8d20e0b1aa604c3e677e
James Coliz <jcoliz@outlook.com>
2024-06-03 16:10:22 -0700

    build: dotnet new nunit -o .\LogoSlideMaker.Tests

---
+++ LogoSlideMaker.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace LogoSlideMaker.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
