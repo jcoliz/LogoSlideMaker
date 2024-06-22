0a22adeb9fcfdb4f43d2c4284839cd08d31d80e2
James Coliz <jcoliz@outlook.com>
2024-06-22 14:21:07 -0700

    feat(ui): Render placeholder blocks

--- LogoSlideMaker.WinUi/MainWindow.xaml.cs
+++ LogoSlideMaker.WinUi/MainWindow.xaml.cs
@@ -2,7 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using LogoSlideMaker.Configure;
+using Microsoft.Graphics.Canvas.Svg;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,6 +14,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Tomlyn;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -22,17 +27,115 @@ namespace LogoSlideMaker.WinUi;
 /// </summary>
 public sealed partial class MainWindow : Window
 {
+    private Definition? _definition;
+    private Layout.Layout? _layout;
+
     public MainWindow()
     {
         this.InitializeComponent();
+        this.LoadResources();
+    }
+
+    private void LoadResources()
+    {
+        var filename = "sample.toml";
+        var names = Assembly.GetExecutingAssembly()!.GetManifestResourceNames();
+        var resource = names.Where(x => x.Contains($".{filename}")).Single();
+        var stream = Assembly.GetExecutingAssembly()!.GetManifestResourceStream(resource);
+        var sr = new StreamReader(stream!);
+        var toml = sr.ReadToEnd();
+        _definition = Toml.ToModel<Definition>(toml);
+
+        _layout = new Layout.Layout(_definition, new Variant());
+        _layout.Populate();
     }
 
     void CanvasControl_Draw(
         Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender,
         Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
     {
+        // Draw a white background
         args.DrawingSession.FillRectangle(new Rect() { X = 96, Y = 96, Width = 1280, Height = 720 }, Microsoft.UI.Colors.White);
-        args.DrawingSession.DrawEllipse(155, 115, 80, 30, Microsoft.UI.Colors.Black, 3);
-        args.DrawingSession.DrawText("Hello, Win2D World!", 100, 100, Microsoft.UI.Colors.Yellow);
+
+        // Render each logo in the layout
+
+        if (_definition is null)
+        {
+            return;        
+        }
+
+        var config = _definition.Render;
+        foreach (var boxlayout in _layout)
+        {
+            foreach (var logolayout in boxlayout.Logos)
+            {
+                var logo = logolayout.Logo;
+
+                if (logo is null)
+                {
+                    continue;
+                }
+
+                {
+                    //using var stream = new FileStream(logo.Path, FileMode.Open);
+                    //var doc = await CanvasSvgDocument.LoadAsync(sender, (Windows.Storage.Streams.IRandomAccessStream)stream);
+
+                    //doc.
+                    //args.DrawingSession.DrawSvg( (doc);
+                }
+
+                //var pic = target.OfType<IPicture>().Last();
+
+                // Adjust size of icon depending on size of source image. The idea is all
+                // icons occupy the same number of pixel area
+
+                var aspect = 1.0m; // pic.Width / pic.Height;
+                var width_factor = (decimal)Math.Sqrt((double)aspect);
+                var height_factor = 1.0m / width_factor;
+                var icon_width = config.IconSize * width_factor * logo.Scale;
+                var icon_height = config.IconSize * height_factor * logo.Scale;
+
+                var X = (logolayout.X - icon_width / 2.0m) * config.Dpi;
+                var Y = (logolayout.Y - icon_height / 2.0m) * config.Dpi;
+                var Width = icon_width * config.Dpi;
+                var Height = icon_height * config.Dpi;
+
+                // Draw a placeholder logo
+                args.DrawingSession.FillRectangle(new Rect() { X = 96.0f + (float)X, Y = 96.0f + (float)Y, Width = (float)Width, Height = (float)Height}, Microsoft.UI.Colors.Red);
+
+                var text_width_inches = logo.TextWidth ?? config.TextWidth;
+
+                var text_x = (logolayout.X - text_width_inches / 2.0m) * config.Dpi;
+                var text_y = (logolayout.Y - config.TextHeight / 2.0m + config.TextDistace) * config.Dpi;
+                var text_width = text_width_inches * config.Dpi;
+                var text_height = config.TextHeight * config.Dpi;
+
+                //target.AddRectangle(100, 100, 100, 100);
+                //var shape = target.Last();
+
+                var shape = new Rect();
+                shape.X = 96.0f + (float)text_x;
+                shape.Y = 96.0f + (float)text_y;
+                shape.Width = (float)text_width;
+                shape.Height = (float)text_height;
+
+                // Draw a placeholder textbox
+                args.DrawingSession.FillRectangle(shape, Microsoft.UI.Colors.Blue);
+
+#if false
+                var tf = shape.TextFrame;
+                tf.Text = logo.Title;
+                tf.LeftMargin = 0;
+                tf.RightMargin = 0;
+                var font = tf.Paragraphs.First().Portions.First().Font;
+
+                font.Size = config.FontSize;
+                font.LatinName = config.FontName;
+                font.Color.Update(config.FontColor);
+                shape.Fill.SetNoFill();
+                shape.Outline.SetNoOutline();
+#endif
+            }
+        }
     }
 }
