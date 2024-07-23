776a8f7048d42f2de1209f5b7c29d83f356a7345
James Coliz <jcoliz@outlook.com>
2024-07-23 09:39:27 -0700

    feat(config): Add 'include.logos' to files config
    Starting #39

--- LogoSlideMaker.Lib/Configure/FilesConfig.cs
+++ LogoSlideMaker.Lib/Configure/FilesConfig.cs
@@ -10,7 +10,16 @@ public record FilesConfig
     /// </summary>
     public string? Output { get; set; } = null;
 
+    /// <summary>
+    /// Configuration for what the slides should look like before
+    /// logos are added
+    /// </summary>
     public TemplateConfig Template { get; set; } = new();
+
+    /// <summary>
+    /// Configuration for what files should be included into this definition
+    /// </summary>
+    public IncludeConfig Include { get; set; } = new();
 }
 
 public record TemplateConfig
@@ -21,4 +30,12 @@ public record TemplateConfig
     public string? Slides { get; set; }
 
     public List<string> Bitmaps { get; set; } = new();
+}
+
+public record IncludeConfig
+{
+    /// <summary>
+    /// Filename of definition where we should extract logos from
+    /// </summary>
+    public string? Logos { get; set; }
 }
---
+++ LogoSlideMaker.Tests/FilesConfigTests.cs
@@ -0,0 +1,34 @@
+﻿using LogoSlideMaker.Configure;
+using LogoSlideMaker.Layout;
+using System.Reflection;
+using Tomlyn;
+
+namespace LogoSlideMaker.Tests;
+
+public class FilesConfigTests
+{
+    /// <summary>
+    /// Scenario: Definition with include.logos loads ok
+    /// </summary>
+    [Test]
+    public void LoadsOk()
+    {
+        // When: Loading a definition file with "include.logos" line
+        var definition = Load("include-logos.toml");
+
+        // Then: Logos filename shows up in the layout
+        Assert.That(definition.Files.Include.Logos, Is.EqualTo("two-pages.toml"));
+    }
+
+    private static Definition Load(string filename)
+    {
+        var names = Assembly.GetExecutingAssembly()!.GetManifestResourceNames();
+        var resource = names.Where(x => x.Contains($".{filename}")).Single();
+        var stream = Assembly.GetExecutingAssembly()!.GetManifestResourceStream(resource);
+        var sr = new StreamReader(stream!);
+        var toml = sr.ReadToEnd();
+        var definitions = Toml.ToModel<Definition>(toml);
+
+        return definitions;
+    }
+}
