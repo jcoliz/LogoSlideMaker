86706bea9859c1c38b92642e1cee205e6c65e9e5
James Coliz <jcoliz@outlook.com>
2025-01-12 11:16:00 -0800

    feat(ui): Change to hand cursor on canvas

---
+++ LogoSlideMaker.WinUi/Controls/CursorGrid.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Input;
+using Microsoft.UI.Xaml.Controls;
+
+namespace LogoSlideMaker.WinUi.Controls;
+
+/// <summary>
+/// A grid with its own cursor.
+/// </summary>
+/// <seealso href="https://stackoverflow.com/questions/76578003/how-can-i-change-the-pointer-of-my-cursor-in-winui3" />
+internal class CursorGrid: Grid
+{
+    public CursorGrid()
+    {
+        this.ProtectedCursor = InputSystemCursor.Create(InputSystemCursorShape.Hand);
+    }
+}
