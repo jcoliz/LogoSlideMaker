cc08d4e9af08943563bbe38b95864a79c53a5c45
James Coliz <jcoliz@outlook.com>
2025-01-13 22:30:37 -0800

    refactor: Simplify new export pipeline

--- LogoSlideMaker.Lib/Export/ExportPipelineEx.cs
+++ LogoSlideMaker.Lib/Export/ExportPipelineEx.cs
@@ -1,5 +1,4 @@
 ﻿using LogoSlideMaker.Configure;
-using LogoSlideMaker.Layout;
 using LogoSlideMaker.Primitives;
 using LogoSlideMaker.Public;
 using ShapeCrawler;
@@ -14,22 +13,9 @@ namespace LogoSlideMaker.Export;
 /// This is an attempt to package the exporting currently done in the
 /// command-line tool, and make it accessible to the UI.
 /// </remarks>
-public class ExportPipelineEx
+public class ExportPipelineEx(IDefinition definition, ImageCache imageCache)
 {
-    public ExportPipelineEx(IDefinition __definition)
-    {
-        definition = __definition;
-        imageCache = new();
-        renderEngine = new(/* TODO Need to get render data here*/new(), imageCache);
-    }
-
-    public async Task LoadAndMeasureAsync(string? basePath)
-    {
-        imageCache.BaseDirectory = basePath;
-        await imageCache.LoadAsync(definition.ImagePaths);
-    }
-
-    public void Save(Stream? templateStream, string outputPath, string? dataVersion)
+    public void Export(Stream? templateStream, string outputPath, string? dataVersion)
     {
         // Open template or create new presentation
         var pres = templateStream is not null ? new Presentation(templateStream) : new Presentation();
@@ -40,8 +26,8 @@ public class ExportPipelineEx
         // RUn each variant through pipeline
         foreach (var variant in definition.Variants)
         {
-            // Render
-            renderEngine.Render(pres, variant, dataVersion);
+            var renderer = new ExportRenderEngineEx(pres, variant, imageCache, dataVersion);
+            renderer.Render();
         }
 
         // Delete template slides off top of result
@@ -54,8 +40,164 @@ public class ExportPipelineEx
         // Save the resulting presentation
         pres.SaveAs(outputPath);
     }
+}
 
-    private readonly IDefinition definition;
-    private readonly ImageCache imageCache;
-    private readonly ExportRenderEngine renderEngine;
+internal class ExportRenderEngineEx(Presentation pres, IVariant variant, ImageCache imageCache, string? dataVersion)
+{
+    internal void Render()
+    {
+        // Generate primitives, but we are only goint to render 'base' primitives to PPTX
+        var primitives = variant
+            .GeneratePrimitives(imageCache)
+            .Where(x => x.Purpose == PrimitivePurpose.Base)
+            .ToList();
+
+        var copyingSlide = pres.Slides[variant.Source];
+        pres.Slides.Add(copyingSlide);
+        var slide = pres.Slides[^1];
+
+        //
+        // Update Description Field
+        //
+
+        SetDescription(slide.Shapes);
+
+        //
+        // Set Slide Notes
+        //
+
+        List<string> notes = [$"Updated: {DateTime.Now:M/dd/yyyy h:mm tt K}"];
+        if (dataVersion is not null)
+        {
+            notes.Add($"Version: {dataVersion}");
+        }
+        notes.AddRange(variant.Notes);
+        slide.AddNotes(notes);
+
+        //
+        // Draw primitives
+        //
+
+        foreach (var p in primitives)
+        {
+            Draw(p, slide.Shapes);
+        }
+    }
+
+    private void SetDescription(ISlideShapes target)
+    {
+        // Fill in description field
+        var num_description_lines = variant.Description.Count;
+        if (num_description_lines > 0)
+        {
+            var description_box = target.TryGetByName<IShape>("Description");
+            if (description_box is not null)
+            {
+                var tf = description_box.TextBox;
+
+                // Ensure there are enough paragraphs to insert text
+                while (tf.Paragraphs.Count < num_description_lines)
+                {
+                    tf.Paragraphs.Add();
+                }
+
+                var queue = new Queue<string>(variant.Description);
+                foreach(var para in tf.Paragraphs)
+                {
+                    if (queue.Count == 0)
+                    {
+                        break;
+                    }
+                    para.Text = queue.Dequeue();
+                }
+            }
+        }        
+    }
+
+    private void Draw(Primitive primitive, ISlideShapes target)
+    {
+        switch (primitive)
+        {
+            case TextPrimitive text:
+                Draw(text, target);
+                break;
+
+            case ImagePrimitive image:
+                Draw(image, target);
+                break;
+
+            case RectanglePrimitive rect:
+                Draw(rect, target);
+                break;
+
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    private void Draw(TextPrimitive primitive, ISlideShapes target)
+    {
+        if (primitive.Style == TextSyle.Invisible)
+            return;
+
+        target.AddShape(100, 100, 100, 100);
+        var shape = target[^1];
+
+        shape.X = primitive.Rectangle.X;
+        shape.Y = primitive.Rectangle.Y ?? 0;
+        shape.Width = primitive.Rectangle.Width;
+        shape.Height = primitive.Rectangle.Height ?? primitive.Rectangle.Width;
+
+        var tf = shape.TextBox;
+        tf.Text = primitive.Text;
+        tf.LeftMargin = 0;
+        tf.RightMargin = 0;
+        var font = tf.Paragraphs[0].Portions[0].Font;
+
+        if (font is not null)
+        {
+            if (variant.TextStyles.TryGetValue(primitive.Style, out var textStyle))
+            {
+                font.Size = textStyle.FontSize;
+                font.LatinName = textStyle.FontName;
+                font.Color.Update(textStyle.FontColor);
+            }
+            else
+            {
+                throw new Exception($"Unsupported text style {primitive.Style}");
+            }
+        }
+
+        shape.Fill.SetNoFill();
+        shape.Outline.SetNoOutline();
+    }
+
+    private void Draw(ImagePrimitive primitive, ISlideShapes target)
+    {
+        var buffer = imageCache.GetOrDefault(primitive.Path) ?? throw new KeyNotFoundException($"No image data found for {primitive.Path}");
+
+        using var stream = new MemoryStream(buffer);
+        target.AddPicture(stream);
+        var pic = target.OfType<IPicture>().Last();
+        pic.X = primitive.Rectangle.X;
+        pic.Y = primitive.Rectangle.Y ?? 0;
+        pic.Width = primitive.Rectangle.Width;
+        pic.Height = primitive.Rectangle.Height ?? primitive.Rectangle.Width;
+    }
+
+    private static void Draw(RectanglePrimitive primitive, ISlideShapes target)
+    {
+        target.AddShape(100, 100, 100, 100);
+        var shape = target[^1];
+
+        shape.X = primitive.Rectangle.X;
+        shape.Y = primitive.Rectangle.Y ?? 0;
+        shape.Width = primitive.Rectangle.Width;
+        shape.Height = primitive.Rectangle.Height ?? primitive.Rectangle.Width;
+
+        if (! primitive.Fill)
+        {
+            shape.Fill.SetNoFill();
+        }
+    }
 }
--- LogoSlideMaker.Lib/Export/Renderer.cs
+++ LogoSlideMaker.Lib/Export/Renderer.cs
@@ -16,47 +16,6 @@ namespace LogoSlideMaker.Export;
 /// </remarks>
 public class ExportRenderEngine(RenderConfig config, ImageCache imageCache)
 {
-    public void Render(Presentation pres, IVariant variant, string? dataVersion)
-    {
-        // Generate primitives, but we are only goint to render 'base' primitives to PPTX
-        var primitives = variant
-            .GeneratePrimitives(imageCache)
-            .Where(x => x.Purpose == PrimitivePurpose.Base)
-            .ToList();
-
-        var copyingSlide = pres.Slides[variant.Source];
-        pres.Slides.Add(copyingSlide);
-        var slide = pres.Slides[^1];
-
-        //
-        // Update Description Field
-        //
-
-        SetDescription(variant.Description, slide.Shapes);
-
-        //
-        // Set Slide Notes
-        //
-
-        List<string> notes = [$"Updated: {DateTime.Now:M/dd/yyyy h:mm tt K}"];
-        if (dataVersion is not null)
-        {
-            notes.Add($"Version: {dataVersion}");
-        }
-        notes.AddRange(variant.Notes);
-        slide.AddNotes(notes);
-
-        //
-        // Draw primitives
-        //
-
-        TextStyles = variant.TextStyles; // Not the best way to get this in, is it??
-        foreach (var p in primitives)
-        {
-            DrawEx(p, slide.Shapes);
-        }
-    }
-
     public void Render(Presentation pres, SlideLayout layout, string? dataVersion, IEnumerable<Primitive> primitives)
     {
         var copyingSlide = pres.Slides[layout.Variant.Source];
@@ -119,94 +78,6 @@ public class ExportRenderEngine(RenderConfig config, ImageCache imageCache)
                 }
             }
         }        
-    }
-
-    private static void SetDescription(IVariant variant, ISlideShapes target)
-    {
-        // Fill in description field
-        var num_description_lines = variant.Description.Count;
-        if (num_description_lines > 0)
-        {
-            var description_box = target.TryGetByName<IShape>("Description");
-            if (description_box is not null)
-            {
-                var tf = description_box.TextBox;
-
-                // Ensure there are enough paragraphs to insert text
-                while (tf.Paragraphs.Count < num_description_lines)
-                {
-                    tf.Paragraphs.Add();
-                }
-
-                var queue = new Queue<string>(variant.Description);
-                foreach (var para in tf.Paragraphs)
-                {
-                    if (queue.Count == 0)
-                    {
-                        break;
-                    }
-                    para.Text = queue.Dequeue();
-                }
-            }
-        }
-    }
-
-    private void DrawEx(Primitive primitive, ISlideShapes target)
-    {
-        switch (primitive)
-        {
-            case TextPrimitive text:
-                DrawEx(text, target);
-                break;
-
-            case ImagePrimitive image:
-                Draw(image, target);
-                break;
-
-            case RectanglePrimitive rect:
-                Draw(rect, target);
-                break;
-
-            default:
-                throw new NotImplementedException();
-        }
-    }
-
-    private void DrawEx(TextPrimitive primitive, ISlideShapes target)
-    {
-        if (primitive.Style == TextSyle.Invisible)
-            return;
-
-        target.AddShape(100, 100, 100, 100);
-        var shape = target[^1];
-
-        shape.X = primitive.Rectangle.X;
-        shape.Y = primitive.Rectangle.Y ?? 0;
-        shape.Width = primitive.Rectangle.Width;
-        shape.Height = primitive.Rectangle.Height ?? primitive.Rectangle.Width;
-
-        var tf = shape.TextBox;
-        tf.Text = primitive.Text;
-        tf.LeftMargin = 0;
-        tf.RightMargin = 0;
-        var font = tf.Paragraphs[0].Portions[0].Font;
-
-        if (font is not null)
-        {
-            if (TextStyles.TryGetValue(primitive.Style, out var textStyle))
-            {
-                font.Size = textStyle.FontSize;
-                font.LatinName = textStyle.FontName;
-                font.Color.Update(textStyle.FontColor);
-            }
-            else
-            {
-                throw new Exception($"Unsupported text style {primitive.Style}");
-            }
-        }
-
-        shape.Fill.SetNoFill();
-        shape.Outline.SetNoOutline();
     }
 
     private void Draw(Primitive primitive, ISlideShapes target)
@@ -302,6 +173,4 @@ public class ExportRenderEngine(RenderConfig config, ImageCache imageCache)
             shape.Fill.SetNoFill();
         }
     }
-
-    private IReadOnlyDictionary<TextSyle, ITextStyle> TextStyles = new Dictionary<TextSyle, ITextStyle>();
 }
