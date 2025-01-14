fd2443a653a68ccbe86ce8c6a448df537b47d71e
James Coliz <jcoliz@outlook.com>
2025-01-13 22:36:28 -0800

    style: Class comments on new public interfaces

--- LogoSlideMaker.Lib/Public/IDefinition.cs
+++ LogoSlideMaker.Lib/Public/IDefinition.cs
@@ -2,19 +2,39 @@
 
 public interface IDefinition
 {
+    /// <summary>
+    /// Title of the overall document
+    /// </summary>
     string? Title { get; }
+
+    /// <summary>
+    /// All the variants (slides) in this document
+    /// </summary>
     ICollection<IVariant> Variants { get; }
 
     /// <summary>
-    /// All the image paths we would need to render
+    /// Paths for all the images we will need to render
     /// </summary>
     ICollection<string> ImagePaths { get; }
 
+    /// <summary>
+    /// Name of the presentation file where we will save to
+    /// </summary>
     string? OutputFileName { get; }
 
+    /// <summary>
+    /// Name of the presentation file containing the base template
+    /// </summary>
     string? TemplateSlidesFileName { get; }
 
+    /// <summary>
+    /// True if the user would like to see a textual listing 
+    /// </summary>
     bool Listing { get; }
 
+    /// <summary>
+    /// Render the slides in textual listing form
+    /// </summary>
+    /// <param name="output"></param>
     void RenderListing(TextWriter output);
 }
--- LogoSlideMaker.Lib/Public/IVariant.cs
+++ LogoSlideMaker.Lib/Public/IVariant.cs
@@ -4,10 +4,35 @@ namespace LogoSlideMaker.Public;
 
 public interface IVariant
 {
+    /// <summary>
+    /// The descriptive name of this slide
+    /// </summary>
     string Name { get; }
+
+    /// <summary>
+    /// The detailed description of what's on this slide
+    /// </summary>
     ICollection<string> Description { get; }
+
+    /// <summary>
+    /// Additional notes which should be included in the slide notes
+    /// </summary>
     ICollection<string> Notes { get; }
+
+    /// <summary>
+    /// The slide number upon which this page should be placed
+    /// </summary>
     int Source { get; }
+
+    /// <summary>
+    /// The styles which should be used to render text on this slide
+    /// </summary>
     IReadOnlyDictionary<TextSyle, ITextStyle> TextStyles { get;}
+
+    /// <summary>
+    /// Generate the primitives needed to render this slide
+    /// </summary>
+    /// <param name="bitmaps"></param>
+    /// <returns></returns>
     ICollection<Primitive> GeneratePrimitives(IGetImageAspectRatio bitmaps);
 }
