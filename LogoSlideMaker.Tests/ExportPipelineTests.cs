8e4fdfa9c09dec149f7bee38b9609949765e5854
James Coliz <jcoliz@outlook.com>
2025-01-16 10:30:56 -0800

    test: Add Scenario: Document title rendered into presentation
    Fixes #60

--- LogoSlideMaker.Tests/ExportPipelineTests.cs
+++ LogoSlideMaker.Tests/ExportPipelineTests.cs
@@ -1,5 +1,6 @@
 using LogoSlideMaker.Export;
 using LogoSlideMaker.Public;
+using Moq;
 using ShapeCrawler;
 
 namespace LogoSlideMaker.Tests;
@@ -27,5 +28,37 @@ internal class ExportPipelineTests: TestsBase
 
         // And: Resulting slide has contents as specified in definition
         Assert.That(presentation.Slides[1].Shapes,Has.Count.EqualTo(4));
+    }
+
+    /// <summary>
+    /// Scenario: Document title rendered into presentation
+    /// </summary>
+    [Test]
+    public void RendersTitle()
+    {
+        // Given: A variant with a unique title
+        var expected = "New document title";
+        var variant = new Mock<IVariant>();        
+        variant.Setup(x=>x.DocumentTitle).Returns(expected);
+        variant.Setup(x=>x.Description).Returns([]);
+        variant.Setup(x=>x.Notes).Returns([]);
+        variant.Setup(x=>x.GeneratePrimitives(It.IsAny<ImageCache>())).Returns([]);
+
+        // And: A presentation with a title block
+        var presentation = new Presentation();
+        var shapes = presentation.Slides[0].Shapes;
+        shapes.AddShape(10,10,10,10);
+        var shape = shapes[^1];
+        shape.Name = "Title";
+        shape.Text = "Old document title";
+
+        // When: Rendering the variant to the presentation
+        var renderer = new ExportRenderEngineEx(presentation, variant.Object, new(), null);
+        renderer.Render();
+
+        // Then: The title appears in the expected shape on the rendered slide
+        shapes = presentation.Slides[^1].Shapes;
+        var titleShape = shapes.TryGetByName<IShape>("Title")!;
+        Assert.That(titleShape.Text,Is.EqualTo(expected));
     }
 }
